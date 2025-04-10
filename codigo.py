3b2ae3dd6c43e5e4ef4ef5e0759a602212f44a7a
Luizrattacaso <rattacasoluiz03@gmail.com>
2025-04-10 19:06:13 -0300

    código base
    Código base para o funcionamento do alarme

---
+++ codigo.py
@@ -0,0 +1,20 @@
+import time
+import datetime
+
+def set_alarm(alarm_time):
+    print(f"Alarme posto para {alarm_time}")
+    is_running = True
+
+    while is_running:
+        hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+        print(hora_atual)
+
+        if alarm_time == hora_atual:
+            print("Hora de acordar!")
+            is_running = False
+
+        time.sleep(1)
+
+if __name__ == "__main__":
+    alarm_time = input("Insira o alarme(Hh:Mm:Ss): ")
+    set_alarm(alarm_time)
