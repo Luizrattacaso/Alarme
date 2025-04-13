c0dcee18a63bc9ce44b951ebe4e271b5bacb26ee
Luizrattacaso <rattacasoluiz03@gmail.com>
2025-04-13 16:13:17 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,18 +1,31 @@
-from email.mime import application
 import sys
-from PyQt5.QtWidgets import QLabel, QWidget, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QMainWindow,QApplication, QLabel, QWidget, QLineEdit, QPushButton
 
-class Interface(QWidget):
+class Interface(QMainWindow):
 
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Alarnme")
+        self.setWindowTitle("Alarme")
+        self.setGeometry(100,100,600,400)
 
-        self.caixa_input = QLineEdit
-        self.set_alarm = QPushButton("Set Alarm")
-        self.parar = QPushButton("Set Alarm")
+        self.caixa_input = QLineEdit(self)
+        self.caixa_input.move(180,50)
+
+        self.set_alarm = QPushButton("Set Alarm", self)
+        self.set_alarm.move(50,50)
+        self.set_alarm.resize(100, 40)
+
+        self.parar = QPushButton("Parar", self)
+        self.parar.move(50,100)
+        self.set_alarm.resize(100, 40)
 
 
-if __name__ == "__name__":
-    app = application(sys.argv)
+def Janela():
+    app = QApplication(sys.argv)
+    janela = Interface()
+    janela.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    Janela()
