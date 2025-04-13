46d36b6fce2e9614e5ac593939df19279211032d
Luizrattacaso <rattacasoluiz03@gmail.com>
2025-04-13 15:31:01 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,3 +1,4 @@
+from email.mime import application
 import sys
 from PyQt5.QtWidgets import QLabel, QWidget, QLineEdit, QPushButton
 
@@ -9,3 +10,9 @@ class Interface(QWidget):
         self.setWindowTitle("Alarnme")
 
         self.caixa_input = QLineEdit
+        self.set_alarm = QPushButton("Set Alarm")
+        self.parar = QPushButton("Set Alarm")
+
+
+if __name__ == "__name__":
+    app = application(sys.argv)
